14e55cf44efa74542098c48bb7d3bd7dbf7cb503
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2023-01-06 03:21:02 +0530

    update: parcel bundler

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello world from React!!"
+  "Hello world from Parcel!!"
 );
 
 const heading2 = React.createElement(
