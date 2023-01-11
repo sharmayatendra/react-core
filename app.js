84b4ec816d284879653ab134e93c89ad125ade6d
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2023-01-12 03:37:30 +0530

    lesson3: laying foundation

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from Parcel!!"
+const heading = (
+  <h1 id="jsx" key="jsx">
+    JSX here
+  </h1>
 );
 
-const heading2 = React.createElement(
-  "h2",
+const nestedHeaderWithJSX = (
+  <div className="title">
+    <h1>i am h1</h1>
+    <h2>i am h2</h2>
+    <h3>i am h3</h3>
+  </div>
+);
+const nestedHeader = React.createElement(
+  "div",
   {
-    id: "heading2",
+    className: "title",
   },
-  "Hello everyone"
+  React.createElement("h1", {}, "I am heading 1!!"),
+  React.createElement("h2", {}, "I am heading 2!!"),
+  React.createElement("h3", {}, "I am heading 3!!")
 );
 
-const container = React.createElement("div", { id: "container" }, [
-  heading,
-  heading2,
-]);
+const Header = () => {
+  return (
+    <div>
+      {heading}
+      <h1>i am header component!!</h1>
+      <h2>i am secondary heading!!</h2>
+    </div>
+  );
+};
+
+const NestedHeader = () => {
+  return (
+    <div className="title">
+      <Header />
+      <h1 className="h1" key="heading1">
+        i am h1 from component.
+      </h1>
+      <h2>i am h2 from component.</h2>
+      <h3>i am h3 from component.</h3>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<NestedHeader />);
