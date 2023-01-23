4a4e1b08a6c20b234323da78cf8b2e85f315f333
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2023-01-24 01:57:59 +0530

    added: Shimmer loading

---
+++ src/components/Shimmer.js
@@ -0,0 +1,21 @@
+const Shimmer = () => {
+  return (
+    <div className="flex">
+      {Array(4)
+        .fill("")
+        .map((_) => (
+          <div className="shimmer-card">
+            <div className="shimmerBG media"></div>
+            <div className="p-32">
+              <div className="shimmerBG title-line"></div>
+              <div className="shimmerBG title-line end"></div>
+              <div className="shimmerBG content-line m-t-24"></div>
+              <div className="shimmerBG content-line"></div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Shimmer;
