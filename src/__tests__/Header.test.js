011bb40e10ff35363781b1ba43e0cea440006f84
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2023-02-23 03:30:16 +0530

    config; configured the testing library

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import Header from "../components/Footer";
+
+test("logo should load on rendering Header", () => {
+  // load header
+  const header = render(<Header />);
+
+  // check if logo is loaded
+});
---
+++ src/__tests__/sum.test.js
@@ -0,0 +1,5 @@
+import { sum } from "../components/sum";
+
+test("should check sum of 2 nums", () => {
+  expect(sum(2, 4)).toBe(6);
+});
